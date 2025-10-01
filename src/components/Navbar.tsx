b7388d1baef9e835feae8ed692a3110aa1dd072f
Emma Fredriksen <emma.fredriksen25@gmail.com>
2025-10-01 20:09:01 +0200

    feat: :iphone: make home page more responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,39 @@
-// src/components/Navbar.tsx
-const Navbar = () => {
-  return (
-    <nav className="absolute pt-16 top-0 left-0 w-full py-4 z-50 text-[24px]">
-      <div className="max-w-6xl mx-auto flex justify-center gap-20 space-x-8">
-        <a href="/" className="transition-colors">About me</a>
-        <a href="/about" className="transition-colors">Skills</a>
-        <a href="/contact" className="transition-colors">Projects</a>
-        <a href="/contact" className="transition-colors">Work</a>
-      </div>
-    </nav>
-  );
-};
+import { useState } from "react";
 
-export default Navbar;
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      {/* Desktop-nav */}
+      <nav className="hidden md:block absolute pt-[4vw] top-0 left-0 w-full py-4 z-50 text-[24px]">
+        <div className="max-w-6xl mx-auto flex justify-center gap-[5vw] space-x-8">
+          <a href="/" className="transition-colors">About me</a>
+          <a href="/about" className="transition-colors">Skills</a>
+          <a href="/contact" className="transition-colors">Projects</a>
+          <a href="/contact" className="transition-colors">Work</a>
+        </div>
+      </nav>
+
+      {/* Hamburger */}
+      <button
+        className="absolute bg-[#AA1515] rounded-2xl flex items-center justify-center w-[15vw] h-[15vw] top-15 right-6 z-[60] text-[2rem] text-white md:hidden"
+        onClick={() => setOpen(!open)}
+      >
+        {open ? "✕" : "☰"}
+      </button>
+
+
+
+      {/* Mobile-nav */}
+      {open && (
+        <div className="fixed top-0 left-0 w-full h-[30vw] bg-[#AA1515] z-50 flex flex-row items-center justify-center gap-8 text-[12px]">
+          <a href="/" className=""onClick={() => setOpen(false)}>About me</a>
+          <a href="/about" onClick={() => setOpen(false)}>Skills</a>
+          <a href="/contact" onClick={() => setOpen(false)}>Projects</a>
+          <a href="/contact" onClick={() => setOpen(false)}>Work</a>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div className="text-center relative pt-[5rem] md:pt-[10rem]">
+        <h1>About me</h1>
+    </div>
+  )
+}
+
+export default About
