2fa396e46afa2f0df173128cfd14ba7e4bb31b30
Emma Fredriksen <emma.fredriksen25@gmail.com>
2025-10-01 22:09:00 +0200

    feat: :sparkles: start on skills page

---
+++ src/pages/Skills.tsx
@@ -0,0 +1,49 @@
+function Skills(){
+    const skills = [
+    { name: "HTML", icon: "src/assets/skills/html.svg" },
+    { name: "CSS", icon: "src/assets/skills/css.svg" },
+    { name: "JavaScript", icon: "src/assets/skills/javascript.svg" },
+    { name: "TypeScript", icon: "src/assets/skills/typescript.svg" },
+    { name: "Python", icon: "src/assets/skills/python.svg" },
+    { name: "Java", icon: "src/assets/skills/java.svg" },
+    { name: "Github", icon: "src/assets/skills/github.svg" },
+    { name: "Git", icon: "src/assets/skills/git.svg" },
+    { name: "Figma", icon: "src/assets/skills/figma.svg" },
+    { name: "Maven", icon: "src/assets/skills/apache.svg" },
+    { name: "React", icon: "src/assets/skills/react.svg" },
+    { name: "Tailwind", icon: "src/assets/skills/tailwind.svg" },
+    { name: "Vite", icon: "src/assets/skills/vite.svg" },
+    { name: "VsCode", icon: "src/assets/skills/vscode.svg" },
+  ];
+    return(
+        <div className="pl-25 pr-25">
+            <div className="skills text-black pt-28">
+                <h1 className="title">MY SKILLS</h1>
+            </div>
+
+        <div className="flex flex-wrap gap-14 mt-12">
+        {skills.map((skill) => (
+            <div
+            key={skill.name}
+            className="flex flex-col items-center group"
+            >
+            <div
+                className="w-32 h-32 rounded-full bg-gray-100 flex items-center justify-center 
+                        transition hover:scale-110 shadow-md"
+            >
+                <img
+                src={skill.icon}
+                alt={skill.name}
+                className="w-15 h-15 filter grayscale group-hover:grayscale-0 transition duration-300"
+                />
+            </div>
+            <p className="mt-3 text-lg font-medium text-gray-700">{skill.name}</p>
+            </div>
+        ))}
+        </div>
+        </div>
+    )
+
+}
+
+export default Skills;
